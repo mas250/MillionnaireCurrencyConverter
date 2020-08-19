8d831af40f38a738de70c5592beca334019df154
mas250 <mas250@exeter.ac.uk>
2020-08-19 15:17:01 +0100

    adding multiple requests

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
 function calculate(){ 
+    
                 var netWorth = document.getElementById("portfolio").value
-                var countries = ["USD", "GBP"]
-                countries.push("EUR")
-                var requestURL = 'https://api.exchangerate.host/convert?from='+countries[1]+'&to='+countries[2]+'&amount='+netWorth ;
-                var request = new XMLHttpRequest();
-
+                var countries = [ "GBP", "USD", "JMD", "JOD", "JPY" ]
                 var richlist = []
+
                 
-                
-                request.open('GET', requestURL);
+                for (i = 1; i < countries.length-1; i++){
+                    
+                    
+                    var requestURL = 'https://api.exchangerate.host/convert?from='+countries[0]+'&to='+countries[i]+'&amount='+netWorth ;
+                    console.log(requestURL)
+
+                    var request = new XMLHttpRequest();
+                    request.open('GET', requestURL);
                 request.responseType = 'json';
                 request.send();
-
-                request.onload = function() {
-                var response = request.response;
-                console.log(response);
+                
+                   
+                 request.onload = function() {
+                    var response = request.response;
+                    richlist.push(response)
+               
+                // if (response) {richlist.push(response.result)}
                 
 
+                 }
+                
+
+                
+                
+              
                 
                 if( (netWorth * 2) > 100 ){
                     document.getElementById("richList").innerHTML = "Yes"
@@ -26,4 +39,5 @@ function calculate(){
                     console.log(netWorth)
                 }
 }
+console.log(richlist);
             }
